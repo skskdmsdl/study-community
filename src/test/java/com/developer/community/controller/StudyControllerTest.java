78a58ef422de920103eb78d0b3eee700102bbf9c
skskdmsdl <skskdmsdl@gmail.com>
2022-08-18 10:46:20 +0900

    스터디 목록, 상세 페이지 호출 테스트

--- src/test/java/com/developer/community/controller/StudyControllerTest.java
+++ src/test/java/com/developer/community/controller/StudyControllerTest.java
@@ -1,5 +1,6 @@
 package com.developer.community.controller;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +28,19 @@ class StudyControllerTest {
         mvc.perform(get("/studies"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("studies/index"))    // view 이름 테스트
                 .andExpect(model().attributeExists("studies"));
+    }
+
+    @Disabled("구현 중")   // 각 단위 method 별로 테스트 제외할 수 있음
+    @DisplayName("[view][GET] 스터디 상세 페이지 - 정상 호출")
+    @Test
+    public void 스터디상세페이지_정상호출() throws Exception {
+        mvc.perform(get("/studies/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("studies/detail"))
+                .andExpect(model().attributeExists("study"))
+                .andExpect(model().attributeExists("studyComments"));
     }
 }
