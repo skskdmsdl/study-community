d13b853409304b95547a4f3e12859c46a91e841f
skskdmsdl <skskdmsdl@gmail.com>
2022-08-19 20:19:32 +0900

    thymeleaf decoupled logic 추가 템플릿 문법을 아예 템플릿에서 분리하여 템플릿 엔진이 작동하지 않아도 렌더링되는 정적 목업 페이지를 만들기 위해 decoupled logic 사용

--- src/main/java/com/developer/community/CommunityApplication.java
+++ src/main/java/com/developer/community/CommunityApplication.java
@@ -3,7 +3,9 @@ package com.developer.community;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class CommunityApplication {
 
