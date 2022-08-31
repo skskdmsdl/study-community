0acac7def0d419bab78c99b1b3adf19498e9ace9
skskdmsdl <skskdmsdl@gmail.com>
2022-08-31 14:55:16 +0900

    메시지 없이 에러코드만 있는 경우 생성자 추가

--- src/main/java/com/developer/community/exception/CommunityApplicationException.java
+++ src/main/java/com/developer/community/exception/CommunityApplicationException.java
@@ -10,6 +10,11 @@ public class CommunityApplicationException extends RuntimeException {
     private ErrorCode errorCode;
     private String message;
 
+    // 메시지 없이 에러코드만 있는 경우의 생성자
+    public CommunityApplicationException(ErrorCode errorCode){
+        this.errorCode = errorCode;
+        this.message = null;
+    }
     @Override
     public String getMessage() {
         if (message == null) {
