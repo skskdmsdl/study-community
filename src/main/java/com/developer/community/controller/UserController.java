a0238702ff7dafa90d51dc91ec93ce8b30a9fe34
skskdmsdl <skskdmsdl@gmail.com>
2022-08-31 15:35:06 +0900

    login controller mapping 및 login 응답을 위한 UserLoginResponse 작성

--- src/main/java/com/developer/community/controller/UserController.java
+++ src/main/java/com/developer/community/controller/UserController.java
@@ -1,12 +1,13 @@
 package com.developer.community.controller;
 
 import com.developer.community.controller.request.UserJoinRequest;
+import com.developer.community.controller.request.UserLoginRequest;
 import com.developer.community.controller.response.Response;
 import com.developer.community.controller.response.UserJoinResponse;
+import com.developer.community.controller.response.UserLoginResponse;
 import com.developer.community.model.User;
 import com.developer.community.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -29,5 +30,11 @@ public class UserController {
         // join
         User user = userService.join(request.getUserName(), request.getPassword());
         return Response.success(UserJoinResponse.fromUser(user));
+    }
+
+    @PostMapping("/login")
+    public Response<UserLoginResponse> login(UserLoginRequest request) {
+        String token = userService.login(request.getUserName(), request.getPassword());
+        return Response.success(new UserLoginResponse(token));
     }
 }
---
+++ src/main/java/com/developer/community/controller/response/UserLoginResponse.java
@@ -0,0 +1,10 @@
+package com.developer.community.controller.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserLoginResponse {
+    private String token;
+}
