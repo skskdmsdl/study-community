f01df3b5f13885a948bd0d57bb366ce8df6e43af
skskdmsdl <skskdmsdl@gmail.com>
2022-09-11 21:01:40 +0900

    study view test code 변경

--- src/test/java/com/developer/community/controller/ViewStudyControllerTest.java
+++ src/test/java/com/developer/community/controller/ViewStudyControllerTest.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,20 +14,17 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View 컨트롤러")
-@WebMvcTest
-class StudyControllerTest {
+@SpringBootTest
+@AutoConfigureMockMvc
+class ViewStudyControllerTest {
 
-    private final MockMvc mvc;
-
-    // test package의 생성자는 Autowired를 생략할 수 없음(생성자가 하나인 경우)
-    public StudyControllerTest(@Autowired MockMvc mvc) {
-        this.mvc = mvc;
-    }
+    @Autowired
+    private MockMvc mockMvc;
 
     @DisplayName("[view][GET] 스터디 리스트 페이지 - 정상 호출")
     @Test
     public void 스터디페이지_정상호출() throws Exception {
-        mvc.perform(get("/studies"))
+        mockMvc.perform(get("/studies"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("studies/index"))    // view 이름 테스트
@@ -36,7 +35,7 @@ class StudyControllerTest {
     @DisplayName("[view][GET] 스터디 상세 페이지 - 정상 호출")
     @Test
     public void 스터디상세페이지_정상호출() throws Exception {
-        mvc.perform(get("/studies/1"))
+        mockMvc.perform(get("/studies/1"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("studies/detail"))
