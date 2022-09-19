dc94cb899bf97cb17c703b74cd28eda08668776b
skskdmsdl <skskdmsdl@gmail.com>
2022-09-19 18:28:23 +0900

    빈 생성자가 없어서 로그인 시 internal server error가 뜸 UserLoginRequest에 @NoArgsConstructor 추가하여 빈 생성자 생성

--- src/main/java/com/developer/community/controller/request/UserLoginRequest.java
+++ src/main/java/com/developer/community/controller/request/UserLoginRequest.java
@@ -2,8 +2,10 @@ package com.developer.community.controller.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class UserLoginRequest {
     private String userName;
