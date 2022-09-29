d6e012026048d47b2afeab6d0dec448821872fac
skskdmsdl <skskdmsdl@gmail.com>
2022-09-29 22:10:10 +0900

    테스트 코드 변경 -> 선택 항목 추가

--- src/test/java/com/developer/community/controller/RestStudyControllerTest.java
+++ src/test/java/com/developer/community/controller/RestStudyControllerTest.java
@@ -31,10 +31,14 @@ class RestStudyControllerTest {
     public void 스터디작성() throws Exception {
         String title = "title";
         String body = "body";
+        Integer recruitment = 0;
+        String subject = "선택안함";
+        Integer onOffline = 0;
+        Integer memberCount = 5;
 
         mockMvc.perform(post("/studies/posts")
                     .contentType(MediaType.APPLICATION_JSON)
-                    .content(objectMapper.writeValueAsBytes(new StudyCreateRequest(title, body)))
+                    .content(objectMapper.writeValueAsBytes(new StudyCreateRequest(title, body, recruitment, subject, onOffline, memberCount)))
                 ).andDo(print())
                 .andExpect(status().isOk());
 
