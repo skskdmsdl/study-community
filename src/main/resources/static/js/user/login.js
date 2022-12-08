5f6b907b49ee8c7ebfe2a0d984b96a9f6ec3d7a2
skskdmsdl <skskdmsdl@gmail.com>
2022-12-08 23:07:35 +0900

    token존재하면 로그아웃 버튼 추가 (작업 중)

--- src/main/resources/static/js/user/login.js
+++ src/main/resources/static/js/user/login.js
@@ -32,7 +32,12 @@ $("#login-btn").click(function() {
     })
     .then((response) => {
         localStorage.setItem('token', response.data.result.token);
+        const token = response.data.result.token;
         console.log(response.data.result.token);
+//        if(token) {
+//            html += '<a class="btn btn-primary order-1 order-lg-0 bg-100 me-lg-2" id="logout">로그아웃</a>';
+//            $('#navbarSupportedContent').html(html);
+//        }
     })
     .catch(() => {
         alert("일치하는 회원정보가 없습니다.");
