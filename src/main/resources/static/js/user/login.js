e3c83c02311d4e32a37bceda57ebf5ba824be2b5
skskdmsdl <skskdmsdl@gmail.com>
2022-12-10 22:20:39 +0900

    주석 제거

--- src/main/resources/static/js/user/login.js
+++ src/main/resources/static/js/user/login.js
@@ -8,22 +8,6 @@ $("#login-btn").click(function() {
         password:$("#login-password").val()
     });
 
-//    $.ajax({
-//        type: "POST",
-//        url: "user/login",
-//        headers: headers,
-//        data: loginInfo
-//    })
-//    .done(function(data) {
-//        // console.log(data.result.token);
-//        // $("#logout-btn").css("display", "block");
-//        alert(data);
-//        console.log(data);
-//    })
-//    .fail(function(data) {
-//        alert("일치하는 회원정보가 없습니다.");
-//    })
-
     axios({
         method: "post",
         url: "/user/login",
@@ -34,6 +18,7 @@ $("#login-btn").click(function() {
         localStorage.setItem('token', response.data.result.token);
         const token = response.data.result.token;
         console.log(response.data.result.token);
+        $("#logout-btn").css("display", "block");
 //        if(token) {
 //            html += '<a class="btn btn-primary order-1 order-lg-0 bg-100 me-lg-2" id="logout">로그아웃</a>';
 //            $('#navbarSupportedContent').html(html);
